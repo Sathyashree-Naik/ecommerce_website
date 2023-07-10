0cbea7433e05a3179ef7a2f2b1de7ab2af2ed82c
Sathyashree-Naik <sathyashreeagnes@gmail.com>
2023-07-10 13:54:57 +0530

    button_design_changes

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -9,32 +9,35 @@ import { styled } from "@mui/material/styles";
 //internal imports
 import "./product.css";
 
-
 //function to change the color of textfield / create a custom textfield
 const CssTextField = styled(TextField)({
-    "& label.Mui-focused": {
-      color: "#343A40 ",
+  "& label.Mui-focused": {
+    color: "#343A40 ",
+  },
+  "& .MuiInput-underline:after": {
+    borderBottomColor: "#343A40 ",
+  },
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderColor: "#343A40 ",
     },
-    "& .MuiInput-underline:after": {
-      borderBottomColor: "#343A40 ",
+    "&:hover fieldset": {
+      borderColor: "#343A40 ",
     },
-    "& .MuiOutlinedInput-root": {
-      "& fieldset": {
-        borderColor: "#343A40 ",
-      },
-      "&:hover fieldset": {
-        borderColor: "#343A40 ",
-      },
-      "&.Mui-focused fieldset": {
-        borderColor: "#343A40 ",
-      },
+    "&.Mui-focused fieldset": {
+      borderColor: "#343A40 ",
     },
-  });
+  },
+});
 
-
-const Checkout = ({ cartList, setCartList, setCheckout,setOpen,setMessage }) => {
-
-  //state variable 
+const Checkout = ({
+  cartList,
+  setCartList,
+  setCheckout,
+  setOpen,
+  setMessage,
+}) => {
+  //state variable
   const [address, setAddress] = useState("");
   const [landmark, setLandmark] = useState("");
   const [country, setCountry] = useState("");
@@ -50,9 +53,7 @@ const Checkout = ({ cartList, setCartList, setCheckout,setOpen,setMessage }) => 
     zipcode: "",
   });
 
-
-  
-//Function to handle checkout
+  //Function to handle checkout
   const orderHandler = (e) => {
     e.preventDefault();
     if (address === "") {
@@ -75,7 +76,6 @@ const Checkout = ({ cartList, setCartList, setCheckout,setOpen,setMessage }) => 
     }
   };
 
-  
   return (
     <div>
       <Box
@@ -98,14 +98,30 @@ const Checkout = ({ cartList, setCartList, setCheckout,setOpen,setMessage }) => 
               <Grid item xs={12} sm container>
                 <Grid item xs container direction="column" spacing={2}>
                   <Grid item xs>
-                    <p style={{ color:"#343A40" ,fontSize: "28px", fontWeight: "bold" ,margin:"auto" ,display:"flex",justifyContent:"center"}}>
+                    <p
+                      style={{
+                        color: "#343A40",
+                        fontSize: "28px",
+                        fontWeight: "bold",
+                        margin: "auto",
+                        display: "flex",
+                        justifyContent: "center",
+                      }}
+                    >
                       Checkout
                     </p>
-                    <div style={{ margin: "16px" }}>
-                      <div>Name :{sessionStorage.getItem("name")}</div>
-                      <div>Email :{sessionStorage.getItem("email")}</div>
-                      <div>
-                        Mobile Number :{sessionStorage.getItem("phone")}
+                    <div style={{ margin: "0px 19px" }}>
+                      <div className="checkout-details">
+                        <p className="checkout-data-header">Fullname: &nbsp;</p>
+                        <p className="checkout-data"> {sessionStorage.getItem("name").charAt(0).toUpperCase() + sessionStorage.getItem("name").slice(1) }</p>
+                      </div>
+                      <div className="checkout-details">
+                        <p className="checkout-data-header">Email: &nbsp;</p>
+                        <p className="checkout-data"> {sessionStorage.getItem("email")}</p>
+                      </div>
+                      <div className="checkout-details">
+                        <p className="checkout-data-header">Mobile:&nbsp; </p>
+                        <p className="checkout-data">{ sessionStorage.getItem("phone")}</p>
                       </div>
                     </div>
                     <div style={{ margin: "10px", padding: "10px" }}>
@@ -214,7 +230,6 @@ const Checkout = ({ cartList, setCartList, setCheckout,setOpen,setMessage }) => 
                       <CssTextField
                         id="custom-css-outlined-input"
                         required
-                      
                         fullWidth
                         label="Zipcode"
                         defaultValue={zipcode}
@@ -246,7 +261,6 @@ const Checkout = ({ cartList, setCartList, setCheckout,setOpen,setMessage }) => 
           </Paper>
         </div>
       </Box>
-    
     </div>
   );
 };
